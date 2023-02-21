e535f793db9cbd42691fb8dd71fdfad4871949bc
Ddevil856 <anshbhimani2003@gmail.com>
2023-02-21 09:16:10 +0530

    Changes
    removed class files

--- Structural Design Pattern/Facade Pattern/AutomobileFacadePattern.java
+++ Structural Design Pattern/Facade Pattern/AutomobileFacadePattern.java
@@ -2,6 +2,16 @@ interface Automobile
 {
     public void start();
     public void stop();
+    public void accelerate(float acceleration);
+}
+
+class Car implements Automobile
+{
+    @Override
+    public void start()
+    {
+        System.out.println("Starting ");
+    }
 }
 
 public class AutomobileFacadePattern
