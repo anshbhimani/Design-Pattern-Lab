f75a93f7d251a62f8cf578c8d6edf65b034b0599
Ddevil856 <anshbhimani2003@gmail.com>
2023-03-24 19:43:14 +0530

    Structural Design Pattern
    Updated Fly Weight Pattern

--- Structural Design Pattern/Fly Weight Pattern/TreeExample.java
+++ Structural Design Pattern/Fly Weight Pattern/TreeExample.java
@@ -2,19 +2,19 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-// we use this pattern for creating large nos of objects of a class
-class TreeFlyWeight{
+class TreeFlyWeight
+{
     String Type;
     String color;
 
     public TreeFlyWeight(String Type, String color){
         this.Type= Type;
         this.color=color;
-        System.out.println(Type);
     }
 }
 
-class tree{
+class tree
+{
     int x;
     int y;
     TreeFlyWeight tf;
@@ -25,14 +25,17 @@ class tree{
     public void plantTree(int x, int y){
         this.x=x;
         this.y=y;
-        System.out.println(x + y);
+        System.out.println(tf.Type + " planted at (" + x + " , " + y + ")");
     }
 }
 
 
-class TreeFactory{
+class TreeFactory
+{
+    static int count_neem;
+    static int count_oak;
     Map<String, TreeFlyWeight> tm = new HashMap<String, TreeFlyWeight>();
-    TreeFlyWeight tfw = null; 
+    TreeFlyWeight tfw = null;
 
     public TreeFlyWeight createTree(String Type, String color){
         if(tm.containsKey(Type)){
@@ -40,9 +43,11 @@ class TreeFactory{
         }
         else if(Type.equalsIgnoreCase("Oak_tree")){
             tfw = new TreeFlyWeight("Oak_tree", "brown");
+            count_oak++;
         }
         else if (Type.equalsIgnoreCase("neem_tree")) {
             tfw = new TreeFlyWeight("neem_tree", "green");
+            count_neem++;
         }
         else{
             System.out.println("invalid type of tree!!");
@@ -52,20 +57,39 @@ class TreeFactory{
     }
 }
 
-public class TreeExample {
-    public static void main(String[] args) 
-    {
-        TreeFactory fact1 = new TreeFactory();
-        Scanner sc = new Scanner("System.in");
+public class TreeExample 
+{
+    public static void main(String[] args) {
+        boolean more = true;
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the number of Tree to be created : ");
-        int number_of_trees = sc.nextInt();
-        
-        System.out.println("Enter Name of the Tree you want to create  : ");
-        String name_of_tree = sc.next();
+        while(more) {
 
-        System.out.println("Enter Color of the Tree you want to create  : ");
-        String color_of_tree = sc.next();
-        
+            TreeFactory treeFactory = new TreeFactory();
+
+            System.out.println("enter the number of tree objects to be created: ");
+            int n = sc.nextInt();
+            System.out.println("enter the name of tree object: ");
+            String s = sc.next();
+            System.out.println("enter the color of tree object: ");
+            String cl = sc.next();
+
+            System.out.println("enter x position of tree object: ");
+            int x = sc.nextInt();
+            System.out.println("enter y position of tree object: ");
+            int y = sc.nextInt();
+
+            for (int i = 0; i < n; i++) 
+            {
+                TreeFlyWeight TreeFlyweight = treeFactory.createTree(s, cl);
+                tree t1 = new tree(TreeFlyweight);
+                t1.plantTree(x++,y++);
+            }
+
+            System.out.println("Do you want to create more tree ? ");
+            more = sc.nextBoolean();
+        }
+
+        System.out.println("maximum density of tree: " + Math.max(TreeFactory.count_neem,TreeFactory.count_oak) );
     }
 }
